8623f4f482a4c2bd5580b273ed934e50d0d14cfd
brendanp5 <brendanp@email.arizona.edu>
2021-04-26 15:10:17 -0700

    Created classes for each individual to work on

---
+++ src/Minesweeper.java
@@ -0,0 +1,10 @@
+import javafx.application.Application;
+
+public class Minesweeper {
+
+	public static void main(String[] args) {
+		Application.launch(MinesweeperView.class, args);
+
+	}
+
+}
---
+++ src/MinesweeperView.java
@@ -0,0 +1,14 @@
+import java.util.Observer;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class MinesweeperView extends Application {
+
+	@Override
+	public void start(Stage arg0) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
